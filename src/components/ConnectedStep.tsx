ac38db4d5cbaeebb0b24b3d84d6380ff51eb8780
Tomas <tomas.sukelovic@gmail.com>
2023-03-28 13:58:37 +0300

    fix: modal transition

--- src/components/ConnectedStep.tsx
+++ src/components/ConnectedStep.tsx
@@ -7,6 +7,7 @@ declare var __TEST__: boolean
 interface Props {
   name: string
   text: string
+  title?: string
   order: number
   tourKey: string
   active?: boolean
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -48,6 +48,8 @@ interface Layout {
 }
 
 interface State {
+  isFirstStep: boolean
+  isLastStep: boolean
   tooltip: object
   notAnimated?: boolean
   containerVisible: boolean
@@ -56,6 +58,7 @@ interface State {
   position?: ValueXY
   tooltipTranslateY: Animated.Value
   opacity: Animated.Value
+  currentStep?: IStep
 }
 
 interface Move {
@@ -86,6 +89,8 @@ export class Modal extends React.Component<ModalProps, State> {
   }
 
   state = {
+    isFirstStep: false,
+    isLastStep: false,
     tooltip: {},
     containerVisible: false,
     tooltipTranslateY: new Animated.Value(400),
@@ -209,6 +214,11 @@ export class Modal extends React.Component<ModalProps, State> {
       useNativeDriver: true,
     })
     this.state.opacity.setValue(0)
+    this.setState({
+      isFirstStep: this.props.isFirstStep,
+      isLastStep: this.props.isLastStep,
+      currentStep: this.props.currentStep,
+    })
     if (
       // @ts-ignore
       toValue !== this.state.tooltipTranslateY._value &&
@@ -300,9 +310,9 @@ export class Modal extends React.Component<ModalProps, State> {
         ]}
       >
         <TooltipComponent
-          isFirstStep={this.props.isFirstStep}
-          isLastStep={this.props.isLastStep}
-          currentStep={this.props.currentStep!}
+          isFirstStep={this.state.isFirstStep}
+          isLastStep={this.state.isLastStep}
+          currentStep={this.state.currentStep!}
           handleNext={this.handleNext}
           handlePrev={this.handlePrev}
           handleStop={this.handleStop}
